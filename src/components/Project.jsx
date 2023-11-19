3e7109a6a698eda5695b75f7c48b5e7d9725e8df
unknown <blancardt@merrimack.edu>
2023-11-18 19:33:26 -0500

    added the Project component.

---
+++ src/components/Project.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+//display the projects onto the page when called.
+function Project({ projectImage, imageTitle, linkDestination, title, description}) {
+  return(
+    <div>
+      <Link to={linkDestination}>
+        <div>
+          <img src={projectImage} alt={imageTitle} />
+        </div>
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default Project;
