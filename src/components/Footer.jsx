6235dde353875e41a2c8d58a5730cbcf434cdc4f
unknown <blancardt@merrimack.edu>
2023-11-20 00:25:54 -0500

    added styling for the footer and fixed some styling for the headed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 //create the footer which contains links to my linkedin and github pages.
 function Footer() {
   return (
-    <footer>
-      <Link to="https://github.com/blanchardt">
+    <footer className='pb-2'>
+      <Link to="https://github.com/blanchardt" className='pe-5'>
         <img
           alt="GitHub"
-          className="img-fluid"
+          className="footer-img-links"
           src="../assets/images/github-mark.png"
         />
       </Link>
       <Link to="https://github.com/blanchardt">
         <img
           alt="LinkedIn"
-          className="img-fluid"
+          className="footer-img-links"
           src="../assets/images/LI-In-Bug.png"
         />
       </Link>
